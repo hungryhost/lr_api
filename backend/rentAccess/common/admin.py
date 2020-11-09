958a2ff13c89d13f9de9557bbacde099be289dfa
Юрий Бородин <yuiborodin@miem.hse.ru>
2020-11-10 02:35:38 +0300

    added common models for logs.

---
+++ backend/rentAccess/common/admin.py
@@ -0,0 +1,5 @@
+from .models import ActionTypes, ResultTypes
+from django.contrib import admin
+
+admin.site.register(ActionTypes)
+admin.site.register(ResultTypes)
--- backend/rentAccess/common/models.py
+++ backend/rentAccess/common/models.py
@@ -6,4 +6,21 @@ from django.db import models
 
 
 class ActionTypes(models.Model):
-	pass
+	"""
+	This model describes types of actions that can be performed.
+	Used for logs only.
+	WARNING: MUST BE FILLED OUT INITIALLY
+	"""
+	act_type = models.CharField(max_length=30, primary_key=True)
+	description = models.CharField(max_length=150)
+
+
+class ResultTypes(models.Model):
+	"""
+	This model describes types of results that can happen after a
+	certain request or action. Used for logs only.
+	WARNING: MUST BE FILLED OUT INITIALLY
+	"""
+	res_type = models.CharField(max_length=30, primary_key=True)
+	description = models.CharField(max_length=150)
+
