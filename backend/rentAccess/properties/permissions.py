35981a1d89bcbd0354e7ad64c1efbb8416ed13fa
Юрий Бородин <yuiborodin@miem.hse.ru>
2020-11-17 00:42:43 +0300

    changed permissions for updated property model.

--- backend/rentAccess/properties/permissions.py
+++ backend/rentAccess/properties/permissions.py
@@ -2,11 +2,16 @@ from django.contrib.auth.models import User
 from rest_framework import permissions
 
 
-# TODO: Update permissions depending on roles
 #
 
 
 class IsOwnerOrSuperuser(permissions.BasePermission):
+	"""
+	Permissions class for properties.
+	Author: Y. Borodin (gitlab: yuiborodin)
+	Version: 1.0
+	Last Update: 16.11.2020
+	"""
 	message = {'Forbidden': ['You do not have necessary permissions']}
 
 	def has_permission(self, request, view):
@@ -14,4 +19,4 @@ class IsOwnerOrSuperuser(permissions.BasePermission):
 
 	# for object level permissions
 	def has_object_permission(self, request, view, obj):
-		return obj.author.id == request.user.id or request.user.is_superuser
+		return obj.author == request.user or request.user.is_superuser
