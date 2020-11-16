b8c928ff7249a0997393d536db08628ff9e7eb54
Юрий Бородин <yuiborodin@miem.hse.ru>
2020-11-17 00:46:11 +0300

    added url paths for each request

--- backend/rentAccess/properties/urls.py
+++ backend/rentAccess/properties/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import PropertyList, PropertyDetail
+from .views import PropertyList, PropertyDetail, PropertyCreate, PropertyUpdate, PropertyDelete
+
+# TODO: separate delete route
+
 urlpatterns = [
 
 	path('<int:pk>/', PropertyDetail.as_view()),
-	path('', PropertyList.as_view()),
-
+	path('list/', PropertyList.as_view()),
+	path('create/', PropertyCreate.as_view()),
+	path('update/<int:pk>/', PropertyUpdate.as_view()),
+	path('delete/<int:pk>/', PropertyDelete.as_view()),
 ]
