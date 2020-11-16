2cb059e4284467c2cc39da91afb211764c88829f
Юрий Бородин <yuiborodin@miem.hse.ru>
2020-11-17 00:58:40 +0300

    changes due to moving AccountTypes model

--- backend/rentAccess/userAccount/admin.py
+++ backend/rentAccess/userAccount/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import (Profile, AccountTypes, UserLogs, UserImages, Phones,
+from .models import (Profile, UserLogs, UserImages, Phones,
 						PhoneTypes)
 
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(AccountTypes)
 admin.site.register(UserImages)
 admin.site.register(UserLogs)
 admin.site.register(PhoneTypes)
