cdd97012809d3596cc18b936342a69e3393740e3
Юрий Бородин <yuiborodin@miem.hse.ru>
2020-11-27 04:46:20 +0300

    new endpoint for uploading an image

--- backend/rentAccess/userAccount/urls.py
+++ backend/rentAccess/userAccount/urls.py
@@ -1,24 +1,36 @@
 from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path, include
-
+from properties.views import (
+    PropertyList,
+    PropertyDetail,
+    PropertyCreate,
+    PropertyUpdate,
+    PropertyDelete
+)
 
 from .views import (
     ProfileDetail,
     ProfileList,
     ChangePasswordView,
     ProfileUpdate,
-    ProfileDelete
-    )
+    ProfileDelete, ProfileUploadUserPic
+)
 
 urlpatterns = [
-    path('<int:pk>/', ProfileDetail.as_view()),
+    path('<int:pk>/', ProfileDetail.as_view(), name='profile_detail'),
     path('change_password/<int:pk>/', ChangePasswordView.as_view(), name='auth_change_password'),
-    path('list/', ProfileList.as_view()),
-    path('update/<int:pk>/', ProfileUpdate.as_view()),
-    path('delete/<int:pk>/', ProfileDelete.as_view()),
+    path('', ProfileList.as_view()),
+    path('<int:pk>/update/', ProfileUpdate.as_view(), name='profile_update'),
+    path('<int:pk>/delete/', ProfileDelete.as_view(), name='profile_delete'),
+    # TODO: consider using endpoints below:
+    # path('<int:pk>/properties/<property_pk>/', PropertyDetail.as_view()),
+    # path('<int:pk>/properties/', PropertyList.as_view()),
+    # path('<int:pk>/properties/create/', PropertyCreate.as_view()),
+    # path('<int:pk>/properties/update/<property_pk>/', PropertyUpdate.as_view()),
+    # path('<int:pk>/properties/delete/<property_pk>/', PropertyDelete.as_view()),
     # TODO: uncomment the paths below after adding the views
-    # path('upload_userpic/<int:pk>/', ProfileUploadUserPic.as_view()),
+    path('<int:pk>/upload_userpic/', ProfileUploadUserPic.as_view()),
     # path('delete_userpic/<int:pk>/', ProfileDeleteUserPic.as_view()),
     # path('reset_password/<int:pk>/', ProfileResetPassword.as_view()),
 ]
