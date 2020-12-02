db67a488ceea737a03432188b836badb70e62fe2
Юрий Бородин <yuiborodin@miem.hse.ru>
2020-12-02 20:25:46 +0300

    fixed urls.py

--- backend/rentAccess/rentAccess/urls.py
+++ backend/rentAccess/rentAccess/urls.py
@@ -27,25 +27,25 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     # ------------------- LOCK-API -------------------
-    path('lock-api/v1/register/', include('register.urls')),
-    path('lock-api/v1/key/', include('keys.urls')),
-    path('lock-api/v1/lock/', include('locks.urls')),
-    path('lock-api/v1/echo/', include('locks.urls')),
-    path('lock-api/v1/access/', include('schedule.urls')),
+    #path('lock-api/v1/register/', include('register.urls')),
+    #path('lock-api/v1/key/', include('keys.urls')),
+    #path('lock-api/v1/lock/', include('locks.urls')),
+    #path('lock-api/v1/echo/', include('locks.urls')),
+    #path('lock-api/v1/access/', include('schedule.urls')),
     # ------------------- SERVICE API -------------------
     path('api/v1/property/', include('properties.urls')),
     # path('auth/', include('rest_framework.urls')),
     path('api/v1/auth/', include('jwtauth.urls'), name='jwtauth'),
     path('api/v1/users/', include('userAccount.urls')),
-    path('api/v1/user/', include('userAccount.urls_v2')),
+    path('api/v1/user/', include('userAccount.urls_v2', namespace='userAccount')),
     path('api/v1/admin/users/', include('userAccount.urls_admin')),
     # ------------------- DOCS -------------------
     path('api/v1/service-api-docs/', TemplateView.as_view(
                       template_name='service-api-docs.html',
                   ), name='swagger-ui'),
-    path('api/v1/lock-api-docs/', TemplateView.as_view(
-                      template_name='lock-api-docs.html',
-                  ), name='swagger-ui'),
+    #path('api/v1/lock-api-docs/', TemplateView.as_view(
+    #                  template_name='lock-api-docs.html',
+    #              ), name='swagger-ui'),
 ] + (
         static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
