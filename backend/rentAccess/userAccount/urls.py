b0d205a11f879b77e2bdd9ee3836bb9263a57044
Юрий Бородин <yuiborodin@miem.hse.ru>
2020-12-12 02:40:42 +0300

    refactored urls for users

--- backend/rentAccess/userAccount/urls.py
+++ backend/rentAccess/userAccount/urls.py
@@ -1,33 +1,17 @@
+
 from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path, include
-from properties.views import (
-    PropertyList,
-    PropertyDetail,
-    PropertyCreate,
-    PropertyUpdate,
-    PropertyDelete
-)
 
-from .views import (
-    ProfileDetail,
-    ProfileList,
-    ChangePasswordView,
-    # ProfileUpdate,
-    # ProfileDelete,
-    ProfileUploadUserPic, DocumentsListCreate
-)
+from .views_v2 import AdminAccessUsersViewSet
+from .views import ProfileList
 
+app_name = 'adminAccessUserAccounts'
 urlpatterns = [
-    path('<int:pk>/', ProfileDetail.as_view(), name='profile_detail'),
     path('', ProfileList.as_view()),
-    # TODO: consider using endpoints below:
-    # path('<int:pk>/properties/<property_pk>/', PropertyDetail.as_view()),
-    # path('<int:pk>/properties/', PropertyList.as_view()),
-    # path('<int:pk>/properties/create/', PropertyCreate.as_view()),
-    # path('<int:pk>/properties/update/<property_pk>/', PropertyUpdate.as_view()),
-    # path('<int:pk>/properties/delete/<property_pk>/', PropertyDelete.as_view()),
-    # TODO: uncomment the paths below after adding the views
-    # path('delete_userpic/<int:pk>/', ProfileDeleteUserPic.as_view()),
-    # path('reset_password/<int:pk>/', ProfileResetPassword.as_view()),
+    path('<int:pk>/', AdminAccessUsersViewSet.as_view({
+        'patch': 'change_username',
+        'get': 'retrieve',
+    }))
+
 ]
