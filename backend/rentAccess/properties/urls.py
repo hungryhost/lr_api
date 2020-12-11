cc573d91977030987b619e5db33e38439dbb1ae7
Юрий Бородин <yuiborodin@miem.hse.ru>
2020-12-12 02:29:02 +0300

    updated urls for current endpoints

--- backend/rentAccess/properties/urls.py
+++ backend/rentAccess/properties/urls.py
@@ -1,18 +1,27 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import PropertyList, PropertyDetail, PropertyCreate, PropertyUpdate, PropertyDelete
+from .views import PropertiesViewSet, PropertyListCreate
+
+app_name = 'properties'
+
+properties_details = PropertiesViewSet.as_view({
+		'patch': 'partial_update',
+		'get': 'retrieve'
+	})
 
 urlpatterns = [
-
-	path('<int:pk>/', PropertyDetail.as_view()),
-	path('list/', PropertyList.as_view()),
-	path('create/', PropertyCreate.as_view()),
-	path('update/<int:pk>/', PropertyUpdate.as_view()),
-	path('delete/<int:pk>/', PropertyDelete.as_view()),
-	# TODO: uncomment the paths below after adding the views
-	# path('upload_main_image/<int:pk>/', PropertyMainImageUpload.as_view()),
-	# path('upload_additional_images/<int:pk>/', PropertyAdditionalImagesUpload.as_view()),
-	# path('delete_main_image/<int:pk>/', PropertyDeleteMainImage.as_view()),
-	# TODO: find a way to either specify the image for delete request ot separate them
-	# path('delete_additional_images/<int:pk/', PropertyDeleteAdditionalImages.as_view()),
+	path('', PropertyListCreate.as_view(), name='properties-list'),
+	path('<int:pk>/owners/<int:owner_id>/', PropertiesViewSet.as_view({
+		'get': 'retrieve_owner',
+		'delete': 'destroy_owner'
+	}), name='owners-details'),
+	path('<int:pk>/owners/', PropertiesViewSet.as_view({
+		'get': 'list_owners',
+		'post': 'add_owner'
+	}), name='owners-list'),
+	path('<int:pk>/', properties_details, name='properties-details'),
+	path('<int:pk>/bookings/', PropertiesViewSet.as_view({
+		'post': 'create_booking',
+		'get': 'list_bookings'
+	}), name='bookings-list'),
 ]
