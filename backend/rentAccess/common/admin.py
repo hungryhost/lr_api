486d681d56b1618853c3052be64946ff85800b01
Юрий Бородин <yuiborodin@miem.hse.ru>
2020-12-22 21:56:38 +0300

    fixed bugs

--- backend/rentAccess/common/admin.py
+++ backend/rentAccess/common/admin.py
@@ -1,5 +1,6 @@
-from .models import ActionTypes, ResultTypes
+from .models import ActionTypes, ResultTypes, PermissionLevels
 from django.contrib import admin
 
 admin.site.register(ActionTypes)
 admin.site.register(ResultTypes)
+admin.site.register(PermissionLevels)
