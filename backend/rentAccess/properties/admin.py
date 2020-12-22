5d539f209425832c36e8f83b7eb0ae2162f7c45a
Юрий Бородин <yuiborodin@miem.hse.ru>
2020-12-22 21:50:35 +0300

    fixed bugs

--- backend/rentAccess/properties/admin.py
+++ backend/rentAccess/properties/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import (Property, PropertyLog, Ownership, PremisesImages)
+from .models import (Property, PropertyLog, Ownership, PremisesImages, PremisesAddresses, PropertyTypes,
+					Bookings)
 # Register your models here.
 app = 'properties'
 admin.site.register(Property)
+admin.site.register(PremisesAddresses)
 admin.site.register(PropertyLog)
 admin.site.register(Ownership)
 admin.site.register(PremisesImages)
+admin.site.register(PropertyTypes)
+admin.site.register(Bookings)
+
