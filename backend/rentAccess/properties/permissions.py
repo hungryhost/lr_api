1324245a6159add82d2a17056540de32a647525a
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-01-10 05:00:07 +0300

    added new permissions for bookings

--- backend/rentAccess/properties/permissions.py
+++ backend/rentAccess/properties/permissions.py
@@ -3,19 +3,25 @@ from rest_framework import permissions
 
 from .models import Ownership, Bookings
 
-r"""
-	Provides a set of permissions for the properties
-		- /properties/ : GET/POST : isAuthenticated
-		- /properties/<properties:id>/owners/
-			- GET: isOwner [with all permission levels]
-			- POST, GET[Retrieve], PATCH, DELETE: isOwner [with permission level 300 or 400]
-		- /properties/<properties:id>/bookings/ : only owners
-"""
+
+class IsSuperUser(permissions.BasePermission):
+	message = {'Forbidden': ['You do not have necessary permissions']}
+
+	def has_permission(self, request, view):
+		return request.user and request.user.is_authenticated
+
+	# for object level permissions
+	def has_object_permission(self, request, view, obj):
+		if request.user.is_superuser:
+			return True
+		return False
 
 
-class IsOwnerOrSuperuser(permissions.BasePermission):
-	"""
-	Permissions class for properties.
+class IsOwner(permissions.BasePermission):
+	r"""
+	object: property
+	This permission class checks whether a user is an owner.
+	Permission level does not matter.
 	"""
 	message = {'Forbidden': ['You do not have necessary permissions']}
 
@@ -24,7 +30,86 @@ class IsOwnerOrSuperuser(permissions.BasePermission):
 
 	# for object level permissions
 	def has_object_permission(self, request, view, obj):
-		if obj.owners.filter(user=request.user).exists() or obj.visibility == 100 or request.user.is_superuser:
+		if obj.owners.filter(user=request.user).exists() or request.user.is_superuser:
+			return True
+		return False
+
+
+class IsClientOfBooking(permissions.BasePermission):
+	r"""
+	obj: booking object
+	"""
+	message = {'Forbidden': ['You do not have necessary permissions']}
+
+	def has_permission(self, request, view):
+		return request.user and request.user.is_authenticated
+
+	# for object level permissions
+	def has_object_permission(self, request, view, obj):
+		if obj.client_email == request.user.email or request.user.is_superuser:
+			return True
+		return False
+
+
+class IsOwnerLevel100(permissions.BasePermission):
+	r"""
+	obj: booking object
+	"""
+	def has_permission(self, request, view):
+		return request.user and request.user.is_authenticated
+
+	def has_object_permission(self, request, view, obj):
+		if (obj.booked_property.owners.filter(user=request.user).exists()
+	and obj.booked_property.owners.get(user=request.user).permission_level_id == 100
+		) or request.user.is_superuser:
+			return True
+		return False
+
+
+class IsOwnerLevel200(permissions.BasePermission):
+	r"""
+	obj: booking object
+	"""
+
+	def has_permission(self, request, view):
+		return request.user and request.user.is_authenticated
+
+	def has_object_permission(self, request, view, obj):
+		if (obj.booked_property.owners.filter(user=request.user).exists()
+	and obj.booked_property.owners.get(user=request.user).permission_level_id == 200
+		) or request.user.is_superuser:
+			return True
+		return False
+
+
+class IsOwnerLevel300(permissions.BasePermission):
+	r"""
+	obj: booking object
+	"""
+
+	def has_permission(self, request, view):
+		return request.user and request.user.is_authenticated
+
+	def has_object_permission(self, request, view, obj):
+		if (obj.booked_property.owners.filter(user=request.user).exists()
+	and obj.booked_property.owners.get(user=request.user).permission_level_id == 300
+		) or request.user.is_superuser:
+			return True
+		return False
+
+
+class IsOwnerLevel400(permissions.BasePermission):
+	r"""
+	obj: booking object
+	"""
+
+	def has_permission(self, request, view):
+		return request.user and request.user.is_authenticated
+
+	def has_object_permission(self, request, view, obj):
+		if (obj.booked_property.owners.filter(user=request.user).exists()
+	and obj.booked_property.owners.get(user=request.user).permission_level_id == 400
+		) or request.user.is_superuser:
 			return True
 		return False
 
@@ -41,11 +126,6 @@ class BookingIsAdminOfPropertyOrSuperuser(permissions.BasePermission):
 		if obj.booked_property.owners.filter(user=request.user).exists() or request.user.is_superuser:
 			return True
 		return False
-
-
-class IsClientUser(permissions.BasePermission):
-	# TODO: complete this permission class in order to use for clients
-	pass
 
 
 class IsInitialOwner(permissions.BasePermission):
