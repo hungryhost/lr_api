22d0e78c2b50cf6799f30bac03f510cb8d1aa4b4
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-01-18 21:04:05 +0000

    Deleted public/favicon.ico, public/index.html, public/logo192.png, public/logo512.png, public/manifest.json, public/robots.txt, src/Components/AuthGuard.js, src/Components/GuestGuard.js, src/Components/Page.js, src/Components/TopBar.js, src/Contexts/Axios.js, src/Contexts/JWT.js, src/Hooks/useAuth.js, src/Slices/Auth.js, src/Slices/Layout.js, src/Store/rootReducer.js, src/Store/store.js, src/Theme/theme.js, src/Utils/auth.js, src/Views/Auth/Components/Login.js, src/Views/Auth/Components/Register.js, src/Views/Auth/AuthView.js, src/Views/Main/NavBar/index.js, src/Views/Main/MainView.js, src/App.js, src/config.js, src/index.js, src/normalize.css, src/routes.js, src/serveServer.js, src/serviceWorker.js files

--- src/Store/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import { ENABLE_REDUX_DEV_TOOLS } from "../config";
-import {rootReducer} from "./rootReducer";
-
-export const store = configureStore({
-    reducer: rootReducer,
-    devTools: ENABLE_REDUX_DEV_TOOLS
-})
