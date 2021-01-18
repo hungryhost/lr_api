243f2f4694cbaae64257db8b1873e22442125be9
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-01-18 21:07:14 +0000

    Update CHANGELOG.md Deleted public/index.html, src/Components/AuthGuard.js, src/Components/GuestGuard.js, src/Components/Page.js, src/Components/TopBar.js, src/Contexts/JWT.js, src/Slices/Auth.js, src/Slices/Layout.js, src/Store/rootReducer.js, src/Theme/theme.js, src/Utils/auth.js, src/Views/Auth/Components/Login.js, src/Views/Auth/Components/Register.js, src/Views/Auth/AuthView.js, src/Views/Main/NavBar/index.js, src/Views/Main/MainView.js, src/App.js, src/config.js, src/normalize.css, src/routes.js, jsconfig.json, package.json, yarn.lock files

--- src/Store/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from '@reduxjs/toolkit'
-import {reducer as auth} from "src/Slices/Auth";
-import {reducer as layout} from 'src/Slices/Layout'
-
-
-export const rootReducer = combineReducers({
-    auth,
-    layout
-})
