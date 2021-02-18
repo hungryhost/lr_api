abefb8ef5ef605f148cfddae364206e0debf8363
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-02-19 01:52:58 +0300

    ADDED custom validation class

---
+++ rentAccess/bookings/custom_validation_errors.py
@@ -0,0 +1,22 @@
+from rest_framework.exceptions import APIException
+from django.utils.encoding import force_text
+from rest_framework import status
+
+
+class CustomValidation(APIException):
+	status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+	default_detail = 'A server error occurred.'
+
+	def __init__(self, detail, field, status_code):
+		if status_code is not None:
+			self.status_code = status_code
+		if detail is not None:
+			self.detail = {}
+			errors = {
+				force_text(detail)
+			}
+			self.detail = {field: errors, 'status_code': self.status_code}
+		else:
+			self.detail = {
+				'detail': force_text(self.default_detail)
+				}
