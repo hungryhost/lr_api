6b49222ef538e18505cc2dd744be765204800c3d
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-02-22 00:13:50 +0300

    UPDATE schema validators

--- rentAccess/api_tests/pytest_coverage/property_tests/e2e_tests/schema_helpers.py
+++ rentAccess/api_tests/pytest_coverage/property_tests/e2e_tests/schema_helpers.py
@@ -1,20 +1,22 @@
 import json
-from os.path import join, dirname
+from os.path import join, dirname, abspath
 from jsonschema import validate
+import logging
 
 
 def assert_valid_schema(data, schema_file):
     """ Checks whether the given data matches the schema """
 
     schema = _load_json_schema(schema_file)
+    logging.info("Loading Schema")
     return validate(data, schema)
 
 
 def _load_json_schema(filename):
     """ Loads the given schema file """
 
-    relative_path = join('schemas', filename)
-    absolute_path = join(dirname(__file__), relative_path)
-
+    absolute_path = join(dirname(abspath(__file__)), 'schemas' + filename)
+    logging.info(absolute_path)
     with open(absolute_path) as schema_file:
+        logging.info("Loading Schema")
         return json.loads(schema_file.read())
