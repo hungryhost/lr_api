94ad2bbd89e12ec6181e11d5dd47517c46c7f95a
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-02-22 00:14:23 +0300

    UPDATE asserts for property create requests

--- rentAccess/api_tests/pytest_coverage/property_tests/e2e_tests/test_crud_properties.py
+++ rentAccess/api_tests/pytest_coverage/property_tests/e2e_tests/test_crud_properties.py
@@ -8,6 +8,7 @@ from common.models import SupportedCity
 from ..factories import PropertyFactory
 import properties.models as p_models
 from .json_factories import PropertyJsonFactory
+from .schema_helpers import assert_valid_schema
 
 r"""
 Since tests are being refactored we now focus on making a class
@@ -35,10 +36,11 @@ class TestCreateProperty:
 	create properties.
 
 	"""
+	@pytest.mark.main
 	@pytest.mark.django_db
-	def test_create(self, db, authenticated_clients, property_list_create_endpoint):
+	def test_daily(self, db, authenticated_clients, property_list_create_endpoint):
 		# firstly we get an instance of PropertyFactory
-		_property = PropertyFactory.build()
+		_property = PropertyFactory.build(booking_type=100)
 		# we then make use of it by getting a dict object
 		_json = PropertyJsonFactory(_property).dump_json()
 		client1 = authenticated_clients.get()
@@ -52,3 +54,92 @@ class TestCreateProperty:
 		assert p_models.Availability.objects.count() == 1
 		assert p_models.PremisesAddress.objects.count() == 1
 		assert response.status_code == 201
+		assert_valid_schema(json.loads(response.content), '\\properties\\post\\property_post_daily.json')
+
+	@pytest.mark.main
+	@pytest.mark.django_db
+	def test_hourly(self, db, authenticated_clients, property_list_create_endpoint):
+		# firstly we get an instance of PropertyFactory
+		_property = PropertyFactory.build(booking_type=200)
+		# we then make use of it by getting a dict object
+		_json = PropertyJsonFactory(_property).dump_json()
+		client1 = authenticated_clients.get()
+		response = client1.post(
+			property_list_create_endpoint,
+			_json,
+			format='json'
+		)
+		assert p_models.Property.objects.count() == 1
+		assert p_models.Ownership.objects.count() == 1
+		assert p_models.Availability.objects.count() == 1
+		assert p_models.PremisesAddress.objects.count() == 1
+		assert response.status_code == 201
+		assert_valid_schema(json.loads(response.content), '\\properties\\post\\property_post_hourly.json')
+
+	@pytest.mark.main
+	@pytest.mark.django_db
+	@pytest.mark.parametrize("price", [
+		100, 200, 1000, 2000, 5000, 10000,
+		pytest.param(-1000, marks=pytest.mark.xfail)])
+	def test_daily_with_price(self, db, price, authenticated_clients, property_list_create_endpoint):
+		# firstly we get an instance of PropertyFactory
+		_property = PropertyFactory.build(booking_type=100, price=price)
+		# we then make use of it by getting a dict object
+		_json = PropertyJsonFactory(_property).dump_json()
+		client1 = authenticated_clients.get()
+		response = client1.post(
+			property_list_create_endpoint,
+			_json,
+			format='json'
+		)
+		assert p_models.Property.objects.count() == 1
+		assert p_models.Ownership.objects.count() == 1
+		assert p_models.Availability.objects.count() == 1
+		assert p_models.PremisesAddress.objects.count() == 1
+		assert response.status_code == 201
+		assert_valid_schema(json.loads(response.content), '\\properties\\post\\property_post_daily.json')
+
+	@pytest.mark.main
+	@pytest.mark.django_db
+	@pytest.mark.parametrize("price", [
+		100, 200, 1000, 2000, 5000, 10000,
+		pytest.param(-1000, marks=pytest.mark.xfail)])
+	def test_hourly_with_price(self, db, price, authenticated_clients, property_list_create_endpoint):
+		# firstly we get an instance of PropertyFactory
+		_property = PropertyFactory.build(booking_type=200, price=price)
+		# we then make use of it by getting a dict object
+		_json = PropertyJsonFactory(_property).dump_json()
+		client1 = authenticated_clients.get()
+		response = client1.post(
+			property_list_create_endpoint,
+			_json,
+			format='json'
+		)
+		assert p_models.Property.objects.count() == 1
+		assert p_models.Ownership.objects.count() == 1
+		assert p_models.Availability.objects.count() == 1
+		assert p_models.PremisesAddress.objects.count() == 1
+		assert response.status_code == 201
+		assert_valid_schema(json.loads(response.content), '\\properties\\post\\property_post_hourly.json')
+
+
+class TestUpdateProperty:
+	r"""
+	These tests are aimed to test only PATCH requests for existing properties.
+	In order to perform these tests following conditions must be met:
+		- A valid JSON schema in directory \\properties\\patch
+		- A fixture for an authenticated user
+		- Fixtures for 2 properties: with daily and hourly booking types
+
+	We do not test permissions here: such tests performed separately and we
+	assume that the authenticated user has appropriate permissions for PATCH requests.
+	"""
+	@pytest.mark.main
+	@pytest.mark.django_db
+	def test_update_daily_main_info(
+			self,
+			db,
+			authenticated_clients,
+			property_list_create_endpoint
+	):
+		pass
