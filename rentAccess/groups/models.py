35934d543ea6b050a608b82ed8c796aef91c8217
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-02-26 03:38:34 +0300

    ADDED: models for property groups

--- rentAccess/groups/models.py
+++ rentAccess/groups/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from properties.models import Property
 # Create your models here.
+from django.conf import settings
+
+
+class PropertyGroup(models.Model):
+	title = models.CharField(max_length=50, null=False, blank=False)
+	description = models.TextField(max_length=500, null=False, blank=False)
+	created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+	updated_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+
+
+class PropertyGroupMembership(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='p_groups',
+		on_delete=models.CASCADE, null=False, blank=False)
+	premises = models.ForeignKey(Property, related_name='groups', on_delete=models.CASCADE, null=False, blank=False)
