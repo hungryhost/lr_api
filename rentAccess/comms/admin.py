dfd5e87d6f5bb096efd2c4035d48ae59676e6eb2
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-03-16 01:47:20 +0300

    ADDED: admin for lock catalog and lock request messages

--- rentAccess/comms/admin.py
+++ rentAccess/comms/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import LockCatalogInfo, LockMessage, LockCatalogImages, LockAvailabilityStorage
 # Register your models here.
+admin.site.register(LockCatalogInfo)
+admin.site.register(LockMessage)
+admin.site.register(LockCatalogImages)
+admin.site.register(LockAvailabilityStorage)
