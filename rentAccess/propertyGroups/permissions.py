3e24146e5454219a103a9fd1124775c3f01af53c
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-03-16 01:50:50 +0300

    ADDED: permissions for groups

---
+++ rentAccess/propertyGroups/permissions.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from rest_framework import permissions
+
+
+# TODO: Update permissions depending on roles
+#
+
+class CanAddPropertyToGroup(permissions.BasePermission):
+	message = {'Forbidden': ['You do not have necessary permissions']}
+
+	def has_permission(self, request, view):
+		return request.user and request.user.is_authenticated
+
+	# for object level permissions
+	def has_object_permission(self, request, view, obj):
+		return obj == request.user or request.user.is_superuser
+
+
+class CanRemovePropertyFromGroup(permissions.BasePermission):
+	message = {'Forbidden': ['You do not have necessary permissions']}
+
+	def has_permission(self, request, view):
+		return request.user and request.user.is_authenticated
+
+	# for object level permissions
+	def has_object_permission(self, request, view, obj):
+		return obj == request.user or request.user.is_superuser
+
