443a14dc6c2b783208e070a6be680864c9c5deb6
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-03-16 01:55:20 +0300

    UPDATED: updated lock-with-property serializers

--- rentAccess/locks/serializers.py
+++ rentAccess/locks/serializers.py
@@ -2,6 +2,8 @@ from django.http import Http404
 from rest_framework import serializers
 from register.models import Lock
 from properties.models import LockWithProperty
+import datetime
+from userAccount.serializers import UserSerializer
 
 
 class EchoSerializer(serializers.ModelSerializer):
@@ -17,11 +19,31 @@ class LockSerializer(serializers.ModelSerializer):
 
 
 class LockAndPropertySerializer(serializers.ModelSerializer):
-	uuid = serializers.UUIDField(format='urn', source='lock.uuid', read_only=True)
+	added_at = serializers.DateTimeField(source='created_at')
+	updated_at = serializers.DateTimeField()
+	last_access_attempt = serializers.SerializerMethodField('get_last_access')
+	added_by = serializers.SerializerMethodField('get_added_by')
+	manufacturing_id = serializers.IntegerField(source='lock.id')
 
 	class Meta:
 		model = LockWithProperty
-		fields = ('id', 'uuid', 'description')
+		fields = (
+			'id',
+			'manufacturing_id',
+			'description',
+			'last_access_attempt',
+			'added_by',
+			'added_at',
+			'updated_at'
+		)
+
+	def get_last_access(self, obj):
+		return datetime.datetime(2020, 3, 1)
+
+	def get_added_by(self, obj):
+		user = obj.added_by
+		serializer = UserSerializer(user)
+		return serializer.data
 
 
 class AddLockToPropertySerializer(serializers.ModelSerializer):
@@ -46,7 +68,10 @@ class AddLockToPropertySerializer(serializers.ModelSerializer):
 			raise serializers.ValidationError(
 				{"Lock": "Lock with given uuid already used."}
 			)
-		lock_and_property = LockWithProperty.objects.create(lock=lock,
-		                                                    property_id=self.context["property_id"],
-		                                                    description=description)
+		lock_and_property = LockWithProperty.objects.create(
+			lock=lock,
+			property_id=self.context["property_id"],
+			description=description,
+			added_by=self.context['request'].user
+		)
 		return lock_and_property
