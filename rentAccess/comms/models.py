14e02e21019757299780bbd13d3f4d68355ec7a5
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-03-16 01:45:40 +0300

    ADDED: models for lock catalog and lock messages

--- rentAccess/comms/models.py
+++ rentAccess/comms/models.py
@@ -5,20 +5,58 @@ from django.conf import settings
 from common.models import SupportedCity
 
 
+class LockCatalogInfo(models.Model):
+	name = models.CharField(max_length=500, null=False, blank=True)
+	description = models.CharField(max_length=3000, null=False, blank=True)
+	price = models.FloatField(null=False, blank=True)
+	delivery = models.BooleanField(null=False, default=True)
+	installation_included = models.BooleanField(null=False, default=True)
+	is_available = models.BooleanField(null=False, default=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class LockCatalogImages(models.Model):
+	lock_info = models.ForeignKey(LockCatalogInfo, related_name='catalog_images', on_delete=models.CASCADE)
+	image = models.ImageField()
+	is_main = models.BooleanField(default=False, null=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class LockAvailabilityStorage(models.Model):
+	lock_info = models.ForeignKey(LockCatalogInfo, related_name='lock_availability',
+	                              on_delete=models.CASCADE)
+	quantity = models.IntegerField(null=False, blank=False)
+	city = models.ForeignKey(SupportedCity, to_field='name', on_delete=models.CASCADE,
+	                         related_name='awailable_locks', blank=False, null=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+
 class SupportMessage(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="support_messages",
-		on_delete=models.CASCADE, null=True, blank=True)
+	                         on_delete=models.CASCADE, null=True, blank=True)
 	title = models.CharField(max_length=255, null=False, blank=True)
 	message = models.CharField(max_length=1000, null=False, blank=True)
 
 
 class LockMessage(models.Model):
+	CHOICES = [
+		('OK', 'Approved'),
+		('FAIL', 'Rejected'),
+		('WAIT', 'Await')
+	]
 	email = models.EmailField(max_length=255, null=False, blank=True)
 	fio = models.CharField(max_length=255, null=False, blank=False)
+	selected_lock = models.ForeignKey(LockCatalogInfo, null=True, blank=True,
+	                                  on_delete=models.CASCADE)
 	phone = PhoneField(blank=True, help_text='Contact phone number')
+	status = models.CharField(max_length=20, null=False, blank=False, default='WAIT', choices=CHOICES)
 	comment = models.CharField(max_length=500, null=False, blank=True)
 	company = models.CharField(max_length=255, null=False, blank=True)
 	quantity = models.IntegerField(default=1, null=False, blank=False)
+	final_price = models.FloatField(null=True, blank=True)
 
 
 class ShippingAddress(models.Model):
@@ -26,7 +64,7 @@ class ShippingAddress(models.Model):
 	Model for storing address for shipping
 	"""
 	shipping_ticket = models.OneToOneField(LockMessage, related_name='shipping_address', on_delete=models.CASCADE,
-									null=True, blank=True)
+	                                       null=True, blank=True)
 	country = models.CharField(max_length=100, blank=False, null=False)
 	city = models.ForeignKey(SupportedCity, to_field='name', on_delete=models.CASCADE,
 	                         related_name='shipping_city', blank=False, null=False)
