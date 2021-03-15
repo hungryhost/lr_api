d6426d313735e48775382250b44be5c100cf3b49
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-03-16 01:51:13 +0300

    ADDED: multiple updates for serializers, see wiki

--- rentAccess/propertyGroups/serializers.py
+++ rentAccess/propertyGroups/serializers.py
@@ -1,13 +1,45 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework import exceptions
 from .models import PropertyGroup, PropertyGroupMembership, UserGroupMembership
+from userAccount.serializers import UserSerializer
+from properties.serializers import PropertyListSerializer
+from properties.models import Property
 
 User = get_user_model()
 
 
-class GroupMemberCreateSerializer(serializers.ModelSerializer):
-	email = serializers.EmailField(required=True)
+class UserGroupMemberListSerializer(serializers.ModelSerializer):
+	user = UserSerializer(many=False, read_only=True)
+	can_add_properties = serializers.BooleanField(required=True)
+	can_delete_properties = serializers.BooleanField(required=True)
+	can_book_properties = serializers.BooleanField(required=True)
+	recursive_ownership = serializers.BooleanField(required=False)
+	can_add_members = serializers.BooleanField(required=True)
+	can_remove_members = serializers.BooleanField(required=True)
+	can_manage_members = serializers.BooleanField(required=True)
 
+	class Meta:
+		model = UserGroupMembership
+		fields = [
+			"user",
+			"can_add_properties",
+			"can_delete_properties",
+			"can_book_properties",
+			"recursive_ownership",
+			"can_add_members",
+			"can_remove_members",
+			"can_manage_members",
+			"created_at",
+			"updated_at"
+		]
+		read_only_fields = [
+			fields
+		]
+
+
+class UserGroupMemberCreateSerializer(serializers.ModelSerializer):
+	email = serializers.EmailField(required=True, source='user.email')
 	can_add_properties = serializers.BooleanField(required=True)
 	can_delete_properties = serializers.BooleanField(required=True)
 	can_book_properties = serializers.BooleanField(required=True)
@@ -36,7 +68,8 @@ class GroupMemberCreateSerializer(serializers.ModelSerializer):
 		]
 
 	def create(self, validated_data):
-		email = validated_data.get("email")
+		user_data = validated_data.get("user")
+		email = user_data.get("email")
 		try:
 			user = User.objects.get(email=email)
 		except User.DoesNotExist:
@@ -74,16 +107,19 @@ class GroupMemberCreateSerializer(serializers.ModelSerializer):
 class GroupCreateSerializer(serializers.ModelSerializer):
 	title = serializers.CharField(max_length=255, required=True, allow_blank=False)
 	description = serializers.CharField(max_length=500, required=True, allow_blank=False)
+	id = serializers.IntegerField(read_only=True)
 
 	class Meta:
 		model = PropertyGroup
 		fields = [
+			"id",
 			"title",
-			"descriptions",
+			"description",
 			"created_at",
 			"updated_at"
 		]
 		read_only_fields = [
+			'id',
 			'created_at',
 			'updated_at'
 		]
@@ -115,3 +151,124 @@ class GroupCreateSerializer(serializers.ModelSerializer):
 		)
 		member.save()
 		return group
+
+
+class GroupListSerializer(serializers.ModelSerializer):
+	is_my_group = serializers.SerializerMethodField("get_is_my_group")
+
+	class Meta:
+		model = PropertyGroup
+		fields = [
+			"id",
+			"title",
+			"is_my_group",
+			"description",
+			"created_at",
+			"updated_at"
+		]
+		read_only_fields = [
+			fields
+		]
+
+	def get_is_my_group(self, obj):
+		user = self.context["request"].user
+		membership = obj.property_groups.all()
+		_member = None
+		for member in membership:
+			if member.user == user:
+				_member = member
+		if _member is not None and _member.is_creator:
+			return True
+		return False
+
+
+class PropertyGroupMemberListSerializer(serializers.ModelSerializer):
+	property = PropertyListSerializer(read_only=True, source='premises')
+	group = GroupListSerializer(read_only=True)
+
+	class Meta:
+		model = PropertyGroupMembership
+		fields = [
+			"id",
+			"group",
+			"property",
+			"created_at",
+			"updated_at"
+		]
+		read_only_fields = [
+			fields
+		]
+
+
+class PropertiesListField(serializers.ListField):
+	child = serializers.IntegerField(min_value=1)
+
+
+class PropertyGroupMemberCreateSerializer(serializers.ModelSerializer):
+	r"""
+	A serializer class we use for adding properties to groups;
+	"""
+	properties = PropertiesListField(allow_null=False, allow_empty=False, required=True)
+
+	class Meta:
+		model = PropertyGroupMembership
+		fields = [
+			"id",
+			"group",
+			"properties",
+			"created_at",
+			"updated_at"
+		]
+		read_only_fields = [
+			"id",
+			"group",
+			"created_at",
+			"updated_at"
+		]
+
+	def create(self, validated_data):
+		properties_input = validated_data.get('properties', None)
+
+		properties_existing = Property.objects.all().prefetch_related(
+			"owners",
+			"owners__user"
+		).filter(id__in=properties_input)
+		if not properties_existing:
+			# here we check whether the properties with given ids exist
+			raise serializers.ValidationError("Properties with given ids do not exist.")
+		if PropertyGroupMembership.objects.all().filter(
+				premises_id__in=properties_input,
+				group_id=self.context['group_id']).exists():
+			# here we check if properties are already in the group
+			raise serializers.ValidationError("Properties already in the group.")
+		# TODO: maybe find a way to move that to views since the permissions should be checked there
+
+		for premises in properties_existing:
+			ownerships = premises.owners.all()
+			# this is step is required to to skip needless queries
+
+			current_user = None
+			# we check whether the current user has permissions to add
+			# the property to groups
+			for owner in ownerships:
+				if owner.user.id == self.context["request"].user.id:
+					current_user = owner
+			if not (current_user and current_user.can_add_to_group):
+				raise exceptions.PermissionDenied
+		# finally we check if the user has permissions to add properties
+		# to the group
+		if not UserGroupMembership.objects.all().filter(
+				user=self.context["request"].user,
+				group_id=self.context['group_id'],
+				can_add_properties=True
+		).exists():
+			raise exceptions.PermissionDenied
+
+		if properties_input:
+			properties_instance = [
+				PropertyGroupMembership(
+					group_id=self.context['group_id'], premises_id=premises)
+				for premises in properties_input]
+			properties = PropertyGroupMembership.objects.bulk_create(properties_instance)
+
+		return properties
