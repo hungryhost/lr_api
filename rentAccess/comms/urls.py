94191d41353bfdb0e4b21d030654dd0db2483212
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-03-17 00:41:59 +0300

    UPDATED: moved lock-requests urls to userAccount

--- rentAccess/comms/urls.py
+++ rentAccess/comms/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from .views import LockMessageListCreate, LockCatalogList, LockMessageRetrieveDeleteView
 
 urlpatterns = [
-    path('lock-requests/', LockMessageListCreate.as_view()),
-    path('lock-requests/<int:pk>/', LockMessageRetrieveDeleteView.as_view()),
     path('lock-catalog/', LockCatalogList.as_view())
 ]
