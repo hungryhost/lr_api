c9ad1aa61c79728fd7ec2222a50ba52b8cdcd236
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-03-23 01:56:58 +0300

    UPDATED: delete urls for properties and members

--- rentAccess/propertyGroups/urls.py
+++ rentAccess/propertyGroups/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework.routers import DefaultRouter
 from .views import GroupListCreateView, UserMembersListCreateView, \
-    PropertyGroupMemberListCreateView, GroupRetrieveUpdateDeleteView
+    PropertyGroupMemberListCreateView, GroupRetrieveUpdateDeleteView, bulk_delete_properties, bulk_delete_members
 
 app_name = 'propertyGroups'
 
@@ -12,5 +12,9 @@ urlpatterns = [
     path('', GroupListCreateView.as_view(), name='groups'),
     path('<int:pk>/', GroupRetrieveUpdateDeleteView.as_view()),
     path('<int:pk>/members/', UserMembersListCreateView.as_view(), name='group-members'),
-    path('<int:pk>/properties/', PropertyGroupMemberListCreateView.as_view(), name='group-properties')
+    path('<int:pk>/members/delete/', bulk_delete_members, name='delete-group-members'),
+
+    path('<int:pk>/properties/', PropertyGroupMemberListCreateView.as_view(), name='group-properties'),
+    path('<int:pk>/properties/delete/', bulk_delete_properties, name='delete-group-properties'),
+
 ]
