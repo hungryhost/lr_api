4d0847095ef501187ce7eb053b06063d4852a9be
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-03-24 19:40:14 +0300

    UPDATED: added new fields and cancel booking serializer

--- rentAccess/bookings/serializers.py
+++ rentAccess/bookings/serializers.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from django.db.models import Q
 from rest_framework import serializers, status
 import logging
+import pytz
 from properties.availability_utils import available_days_from_db, available_hours_from_db, \
 	decompose_incoming_booking
 from properties.logger_helpers import get_client_ip
@@ -11,11 +12,26 @@ from properties.models import Property, Ownership
 from properties.serializers import PropertyListSerializer
 from .models import Booking
 
+
 User = get_user_model()
 crud_logger_info = logging.getLogger('rentAccess.properties.crud.info')
 owners_logger = logging.getLogger('rentAccess.properties.owners.info')
 bookings_logger = logging.getLogger('rentAccess.properties.bookings.info')
 images_logger = logging.getLogger('rentAccess.properties.images.info')
+
+
+class BookedBySerializer(serializers.Serializer):
+	first_name = serializers.CharField(max_length=255)
+	email = serializers.EmailField()
+	class Meta:
+		fields = [
+			'first_name',
+			'email'
+		]
+		read_only_fields = [
+			'first_name',
+			'email'
+		]
 
 
 class DailyBookingCreateFromOwnerSerializer(serializers.ModelSerializer):
@@ -444,10 +460,6 @@ class HourlyBookingCreateFromClientSerializer(serializers.ModelSerializer):
 		return self.context['property'].property_address.city.city.timezone
 
 	def validate(self, attrs):
-		if self.context["request"].user.email == attrs["client_email"]:
-			raise serializers.ValidationError({
-				"client_email": "Cannot book property you own for yourself.",
-			})
 		# we retrieve the property from context in order to reduce sql queries
 		property_to_book = self.context['property']
 
@@ -555,20 +567,25 @@ class BookingsListSerializer(serializers.ModelSerializer):
 	booked_until = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=False, read_only=True)
 	timezone = serializers.CharField(source='booked_property.property_address.city.city.timezone', required=False,
 	                                 read_only=True)
+	available_actions = serializers.SerializerMethodField('get_available_actions')
+	booked_by_info = serializers.SerializerMethodField('get_booked_by_info')
 
 	class Meta:
 		model = Booking
 		fields = (
 			'id',
+			'available_actions',
 			'booked_property',
 			'number_of_clients',
 			'client_email',
 			'status',
+			'cancelled_reason',
 			'price',
 			'timezone',
 			'booked_from',
 			'booked_until',
 			'booked_by',
+			'booked_by_info',
 			'created_at',
 			'updated_at'
 		)
@@ -578,23 +595,75 @@ class BookingsListSerializer(serializers.ModelSerializer):
 			'number_of_clients',
 			'client_email',
 			'status',
+			'cancelled_reason',
 			'price',
 			'timezone',
 			'booked_from',
 			'booked_until',
 			'booked_by',
 			'created_at',
-			'updated_at'
+			'updated_at',
+			'booked_by_info'
 		]
+
+	def to_representation(self, instance):
+		representation = super().to_representation(instance)
+		timezone = instance.booked_property.property_address.city.city.timezone
+		localtz = pytz.timezone(timezone)
+		dt_aware = instance.booked_until.replace(tzinfo=localtz)
+		if dt_aware < datetime.now(tz=localtz):
+			representation['status'] = 'EXPIRED'
+		return representation
+
+	def get_booked_by_info(self, instance):
+		serializer = BookedBySerializer(instance.booked_by)
+		return serializer.data
+
+	def get_available_actions(self, instance):
+		owners = instance.booked_property.owners.all()
+		currently_owner = None
+		status_can_be_updated = False
+		number_of_clients_can_be_updated = False
+		can_be_cancelled_by_user = False
+		for owner in owners:
+			if self.context['request'].user == owner.user:
+				currently_owner = owner
+		if currently_owner:
+			if currently_owner.can_manage_bookings:
+				status_can_be_updated = True
+				can_be_cancelled_by_user = True
+			if instance.booked_by == currently_owner.user:
+				number_of_clients_can_be_updated = True
+				can_be_cancelled_by_user = True
+		if self.context['request'].user.email == instance.client_email:
+			number_of_clients_can_be_updated = True
+			can_be_cancelled_by_user = True
+
+		if instance.status == 'ACCEPTED':
+			status_can_be_updated = False
+
+		timezone = instance.booked_property.property_address.city.city.timezone
+		localtz = pytz.timezone(timezone)
+		dt_aware = instance.booked_until.replace(tzinfo=localtz)
+		if dt_aware < datetime.now(tz=localtz) or \
+				instance.status in ['CANCELLED_BY_CLIENT', 'CANCELLED_BY_OWNER', 'EXPIRED']:
+			status_can_be_updated = False
+			number_of_clients_can_be_updated = False
+			can_be_cancelled_by_user = False
+		return_dict = {
+			"status_can_be_updated": status_can_be_updated,
+			"number_of_clients_can_be_updated": number_of_clients_can_be_updated,
+			"can_be_cancelled_by_user": can_be_cancelled_by_user
+		}
+		return return_dict
 
 
 class BookingUpdateAdminAndCreatorSerializer(serializers.ModelSerializer):
-	status = serializers.CharField(required=True)
 	booked_property = PropertyListSerializer(many=False, read_only=True)
-	number_of_clients = serializers.IntegerField(required=True, max_value=100)
-	booked_from = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=True)
-	booked_until = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=True)
-	timezone = serializers.CharField(source='booked_property.property_address.city.city.timezone', required=False)
+	booked_from = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=False, read_only=True)
+	booked_until = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=False, read_only=True)
+	timezone = serializers.CharField(source='booked_property.property_address.city.city.timezone', required=False, read_only=True)
+	_meta_ = serializers.SerializerMethodField('get_meta_info')
 
 	class Meta:
 		model = Booking
@@ -604,81 +673,46 @@ class BookingUpdateAdminAndCreatorSerializer(serializers.ModelSerializer):
 			'number_of_clients',
 			'client_email',
 			'status',
+			'cancelled_reason',
 			'booked_from',
 			'booked_until',
 			'timezone',
 			'price',
 			'booked_by',
 			'created_at',
-			'updated_at'
+			'updated_at',
+			'_meta_'
 		)
 		read_only_fields = [
-			'timezone',
 			'id',
-			'price',
 			'booked_property',
 			'client_email',
+			'booked_from',
+			'booked_until',
+			'cancelled_reason',
+			'timezone',
+			'price',
 			'booked_by',
 			'created_at',
-			'updated_at']
+			'updated_at',
+			'_meta_'
+		]
 
-	def is_valid(self, raise_exception=False):
-		ret = super(BookingUpdateAdminAndCreatorSerializer, self).is_valid(False)
-		if self._errors:
-			bookings_logger.info(
-				f"object: booking; stage: serialization; action_type: create; "
-				f"user_id: {self.context['request'].user.id}; property_id: {self.context['property_id']}; "
-				f"ip_addr: {get_client_ip(self.context['request'])}; status: NOT OK; serialization failed;")
-			if raise_exception:
-				raise serializers.ValidationError(self.errors)
-		return ret
-
-	def validate(self, attrs):
-		if (not attrs.get("booked_from") and attrs.get("booked_until")) or (
-				attrs.get("booked_from") and not attrs.get("booked_until")
-		):
-			raise serializers.ValidationError({
-				"dates": "Provide both dates",
-			})
-		if attrs["booked_from"] > attrs["booked_until"]:
-			raise serializers.ValidationError({
-				"dates": "Dates are not valid",
-			})
-		if not (self.instance.booked_from == attrs["booked_from"] and self.instance.booked_until == attrs["booked_until"]):
-
-			query_1 = Q()
-			# query_1.add(Q(booked_property_id=1), Q.AND)
-			# query_1.add(Q(booked_from__lte=datetime_start), Q.OR)
-			query_1.add(Q(booked_from__lte=attrs["booked_from"]) & Q(booked_until__gte=attrs["booked_from"]),
-			            query_1.connector)
-			query_1.add(Q(booked_from__lt=attrs["booked_until"]) & Q(booked_until__gte=attrs["booked_until"]), Q.OR)
-			query_1.add(Q(booked_from__gte=attrs["booked_from"]) & Q(booked_from__lte=attrs["booked_until"]), Q.OR)
-			query_1.add(Q(booked_property_id=self.context["property_id"]), Q.AND)
-			query_2 = Q()
-			query_2.add(Q(booked_from=attrs["booked_until"]) | Q(booked_until=attrs["booked_from"]), query_2.connector)
-			queryset = Booking.objects.filter(query_1).exclude(query_2)
-
-			if queryset.exists():
-				raise serializers.ValidationError({
-					"dates": "Cannot book with these dates",
-				}, code=status.HTTP_409_CONFLICT)
-
-		return super(BookingUpdateAdminAndCreatorSerializer, self).validate(attrs)
+	def get_meta_info(self, obj):
+		return {
+			"S_ID": "B-U-AAC-1",
+			"ACT": "PUT",
+			"API": "LR_S_API_100",
+			"TIMESTAMP": datetime.now()
+		}
 
 	def update(self, instance, validated_data):
-		status_ = validated_data.get("status", None)
+		status = validated_data.get("status", None)
 		number_of_clients = validated_data.get("number_of_clients", None)
-		booked_from = validated_data.get("booked_from", None)
-		booked_until = validated_data.get("booked_until", None)
 		if status:
-			instance.status = status_
+			instance.status = status
 		if number_of_clients:
 			instance.number_of_clients = number_of_clients
-		if booked_from:
-			instance.booked_from = booked_from
-		if booked_until:
-			instance.booked_until = booked_until
-
 		instance.save()
 		bookings_logger.info(
 			f"object: booking; stage: serialization; action_type: create; "
@@ -689,11 +723,12 @@ class BookingUpdateAdminAndCreatorSerializer(serializers.ModelSerializer):
 
 
 class BookingUpdateAdminNotCreatorSerializer(serializers.ModelSerializer):
-	status = serializers.CharField(required=True)
-	booked_from = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=True)
-	booked_until = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=True)
-	timezone = serializers.CharField(source='booked_property.property_address.city.city.timezone', required=False)
+	booked_from = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=False, read_only=True)
+	booked_until = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=False, read_only=True)
+	timezone = serializers.CharField(source='booked_property.property_address.city.city.timezone',
+	                                 required=False, read_only=True)
 	booked_property = PropertyListSerializer(many=False, read_only=True)
+	_meta_ = serializers.SerializerMethodField('get_meta_info')
 
 	class Meta:
 		model = Booking
@@ -703,29 +738,43 @@ class BookingUpdateAdminNotCreatorSerializer(serializers.ModelSerializer):
 			'number_of_clients',
 			'client_email',
 			'status',
+			'cancelled_reason',
 			'booked_from',
 			'booked_until',
 			'timezone',
 			'price',
 			'booked_by',
 			'created_at',
-			'updated_at'
+			'updated_at',
+			'_meta_'
 		)
 		read_only_fields = [
-			'timezone',
 			'id',
 			'booked_property',
 			'number_of_clients',
 			'client_email',
 			'booked_from',
 			'booked_until',
-			'booked_by',
+			'timezone',
 			'price',
+			'cancelled_reason',
+			'booked_by',
 			'created_at',
-			'updated_at']
+			'updated_at',
+			'_meta_'
+		]
+
+	def get_meta_info(self, obj):
+		return {
+			"S_ID": "B-U-ANC-1",
+			"ACT": "PUT",
+			"API": "LR_S_API_100",
+			"TIMESTAMP": datetime.now()
+		}
 
 	def update(self, instance, validated_data):
 		status = validated_data.get("status", None)
+
 		if status:
 			instance.status = status
 		instance.save()
@@ -739,9 +788,11 @@ class BookingUpdateAdminNotCreatorSerializer(serializers.ModelSerializer):
 
 class BookingUpdateClientSerializer(serializers.ModelSerializer):
 	number_of_clients = serializers.IntegerField(required=True, max_value=100)
-	booked_from = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=True)
-	booked_until = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=True)
-	timezone = serializers.CharField(source='booked_property.property_address.city.city.timezone', required=False)
+	booked_from = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=False, read_only=True)
+	booked_until = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=False, read_only=True)
+	timezone = serializers.CharField(source='booked_property.property_address.city.city.timezone', required=False, read_only=True)
+	booked_property = PropertyListSerializer(many=False, read_only=True)
+	_meta_ = serializers.SerializerMethodField('get_meta_info')
 
 	class Meta:
 		model = Booking
@@ -751,73 +802,109 @@ class BookingUpdateClientSerializer(serializers.ModelSerializer):
 			'number_of_clients',
 			'client_email',
 			'status',
+			'cancelled_reason',
 			'booked_from',
 			'booked_until',
 			'timezone',
 			'booked_by',
 			'created_at',
-			'updated_at'
+			'updated_at',
+			'_meta_'
 		)
 		read_only_fields = [
-			'timezone',
 			'id',
 			'booked_property',
-			'number_of_clients',
 			'client_email',
-			'booked_from',
 			'status',
+			'booked_from',
 			'booked_until',
+			'cancelled_reason',
+			'timezone',
 			'booked_by',
 			'created_at',
-			'updated_at']
+			'updated_at',
+			'_meta_'
+		]
 
-	def validate(self, attrs):
-		if (not attrs.get("booked_from") and attrs.get("booked_until")) or (
-				attrs.get("booked_from") and not attrs.get("booked_until")
-		):
-			raise serializers.ValidationError({
-				"dates": "Provide both dates",
-			})
-		if attrs["booked_from"] > attrs["booked_until"]:
-			raise serializers.ValidationError({
-				"dates": "Dates are not valid",
-			})
-		if not (self.instance.booked_from == attrs["booked_from"] and self.instance.booked_until == attrs[
-			"booked_until"]):
-
-			query_1 = Q()
-			# query_1.add(Q(booked_property_id=1), Q.AND)
-			# query_1.add(Q(booked_from__lte=datetime_start), Q.OR)
-			query_1.add(Q(booked_from__lte=attrs["booked_from"]) & Q(booked_until__gte=attrs["booked_from"]),
-			            query_1.connector)
-			query_1.add(Q(booked_from__lt=attrs["booked_until"]) & Q(booked_until__gte=attrs["booked_until"]), Q.OR)
-			query_1.add(Q(booked_from__gte=attrs["booked_from"]) & Q(booked_from__lte=attrs["booked_until"]), Q.OR)
-			query_1.add(Q(booked_property_id=self.context["property_id"]), Q.AND)
-			query_2 = Q()
-			query_2.add(Q(booked_from=attrs["booked_until"]) | Q(booked_until=attrs["booked_from"]), query_2.connector)
-			queryset = Booking.objects.filter(query_1).exclude(query_2)
-
-			if queryset.exists():
-				raise serializers.ValidationError({
-					"dates": "Cannot book with these dates",
-				}, code=status.HTTP_409_CONFLICT)
-		return super(BookingUpdateClientSerializer, self).validate(attrs)
+	def get_meta_info(self, obj):
+		return {
+			"S_ID": "B-U-CL-1",
+			"ACT": "PUT",
+			"API": "LR_S_API_100",
+			"TIMESTAMP": datetime.now()
+		}
 
 	def update(self, instance, validated_data):
 		number_of_clients = validated_data.get("number_of_clients", None)
-		booked_from = validated_data.get("booked_from", None)
-		booked_until = validated_data.get("booked_until", None)
 		if number_of_clients:
 			instance.number_of_clients = number_of_clients
-		if booked_from:
-			instance.booked_from = booked_from
-		if booked_until:
-			instance.booked_from = booked_until
-
 		instance.save()
 		bookings_logger.info(
 			f"object: booking; stage: serialization; action_type: create; "
 			f"user_id: {self.context['request'].user.id}; property_id: {self.context['property_id']}; booking_id: {instance.id} "
 			f"ip_addr: {get_client_ip(self.context['request'])}; status: OK;")
 
+		return instance
+
+
+class CancelBookingSerializer(serializers.ModelSerializer):
+	cancelled_reason = serializers.CharField(max_length=255, required=True, allow_blank=False)
+
+	booked_from = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=False, read_only=True)
+	booked_until = serializers.DateTimeField(format="%Y-%m-%dT%H:%M%z", required=False, read_only=True)
+	timezone = serializers.CharField(source='booked_property.property_address.city.city.timezone', required=False,
+	                                 read_only=True)
+	booked_property = PropertyListSerializer(many=False, read_only=True)
+
+	class Meta:
+		model = Booking
+		fields = [
+			'id',
+			'booked_property',
+			'number_of_clients',
+			'client_email',
+			'status',
+			'booked_from',
+			'booked_until',
+			'timezone',
+			'booked_by',
+			'created_at',
+			'cancelled_reason',
+			'updated_at'
+		]
+		read_only_fields = [
+			'id',
+			'booked_property',
+			'number_of_clients',
+			'client_email',
+			'status',
+			'booked_from',
+			'booked_until',
+			'timezone',
+			'booked_by',
+			'created_at',
+			'updated_at',
+		]
+
+	def get_meta_info(self, obj):
+		return {
+			"S_ID"     : "B-U-CL-1",
+			"ACT"      : "PUT",
+			"API"      : "LR_S_API_100",
+			"TIMESTAMP": datetime.now()
+		}
+
+	def update(self, instance, validated_data):
+		if instance.status in ['CANCELLED_BY_CLIENT', 'CANCELLED_BY_OWNER']:
+			raise serializers.ValidationError({
+				"status": "the booking is already cancelled."
+			})
+		cancelled_reason = validated_data.get("cancelled_reason", None)
+		if self.context['request'].user.email == instance.client_email:
+			instance.status = 'CANCELLED_BY_CLIENT'
+		else:
+			instance.status = 'CANCELLED_BY_OWNER'
+		if cancelled_reason:
+			instance.cancelled_reason = cancelled_reason
+		instance.save()
 		return instance
