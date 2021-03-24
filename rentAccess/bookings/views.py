da43541d571eedb776c9542c22b085f69b6100b1
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-03-24 19:40:53 +0300

    UPDATED: filtering and permissions

--- rentAccess/bookings/views.py
+++ rentAccess/bookings/views.py
@@ -10,26 +10,28 @@ from bookings.models import Booking
 from django.db.models import Q, Prefetch
 from django.http import Http404
 from rest_framework import generics, permissions, status, response, serializers, viewsets, mixins, exceptions, \
-	decorators
+	decorators, filters
 from rest_framework.decorators import action, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from register.models import Key
 from .permissions import IsOwnerLevel100, IsOwnerLevel200, IsOwnerLevel300, IsOwnerLevel400, \
-	IsClientOfBooking, BookingIsAdminOfPropertyOrSuperuser, CanRetrieve
+	IsClientOfBooking, BookingIsAdminOfPropertyOrSuperuser, CanRetrieve, CanUpdateBooking
 from rest_framework import generics
 from properties.models import LockWithProperty, Property, Ownership, PremisesImage
 from .serializers import BookingsListSerializer, BookingUpdateAdminAndCreatorSerializer, \
 	BookingUpdateAdminNotCreatorSerializer, BookingUpdateClientSerializer, \
 	DailyBookingCreateFromClientSerializer, DailyBookingCreateFromOwnerSerializer, \
-	HourlyBookingCreateFromClientSerializer, HourlyBookingCreateFromOwnerSerializer
+	HourlyBookingCreateFromClientSerializer, HourlyBookingCreateFromOwnerSerializer, CancelBookingSerializer
 
 bookings_logger = logging.getLogger('rentAccess.properties.bookings.info')
 
 
 class BookingsListCreateView(generics.ListCreateAPIView):
 	serializer_class = BookingsListSerializer
+	filter_backends = [filters.OrderingFilter]
+	ordering_fields = ['booked_until', 'created_at', 'updated_at']
 
 	def create(self, request, *args, **kwargs):
 		user = self.request.user
@@ -77,16 +79,16 @@ class BookingsListCreateView(generics.ListCreateAPIView):
 			"email": obj.client_email,
 		}
 		try:
-			lwp = LockWithProperty.objects.select_related('lock').get(property_id=self.kwargs['pk'])
+			lwp = LockWithProperty.objects.select_related('lock').all().filter(property_id=self.kwargs['pk'])
 		except LockWithProperty.DoesNotExist:
 			lwp = None
-		if lwp:
-			key = Key(lock_id=lwp.lock_id, access_start=obj.booked_from, access_stop=obj.booked_until)
-			key.save()
-			data['key']: key.code
+		#if lwp:
+		#	key = Key(lock_id=lwp.lock_id, access_start=obj.booked_from, access_stop=obj.booked_until)
+		#	key.save()
+		#	data['key']: key.code
 		# send_booking_email_to_client(has_key=True, data=data, duration=0)
-		else:
-			pass
+		#else:
+		#	pass
 		# send_booking_email_to_client(has_key=False, data=data, duration=0)
 
 	def get(self, request, *args, **kwargs):
@@ -101,11 +103,12 @@ class BookingsListCreateView(generics.ListCreateAPIView):
 		return super(self.__class__, self).get(self, request, *args, **kwargs)
 
 	def get_queryset(self, *args, **kwargs):
-		queryset = Booking.objects.prefetch_related(
+		queryset = Booking.objects.all().prefetch_related(
 			'booked_property', 'booked_property__property_address',
 			'booked_property__property_address__city',
-			'booked_property__property_images'
-		).all().filter(booked_property=self.kwargs['pk'], is_deleted=False)
+			'booked_property__property_images',
+			'booked_property__owners__user'
+		).select_related('booked_by').filter(booked_property=self.kwargs['pk'], is_deleted=False)
 		return queryset
 
 	def _get_serializer(self, _property, _owner=None, *args, **kwargs):
@@ -144,6 +147,8 @@ class BookingsListCreateView(generics.ListCreateAPIView):
 
 class BookingsAllList(generics.ListAPIView):
 	serializer_class = BookingsListSerializer
+	filter_backends = [filters.OrderingFilter]
+	ordering_fields = ['booked_until', 'created_at', 'updated_at']
 
 	def get_queryset(self, *args, **kwargs):
 		query = Q()
@@ -154,8 +159,19 @@ class BookingsAllList(generics.ListAPIView):
 			'booked_property__property_images'
 		).all().filter(
 			query
-		)
+		).order_by('booked_from')
 		return queryset
+
+	def get(self, request, *args, **kwargs):
+		try:
+			property_owners = Property.objects.prefetch_related(
+				Prefetch('owners', queryset=Ownership.objects.select_related('user').all())).get(pk=self.kwargs['pk'])
+		except Property.DoesNotExist:
+			raise Http404
+		ownerships = [owner.user for owner in property_owners.owners.all()]
+		if not (self.request.user in ownerships) and self.request.method == "GET":
+			raise exceptions.PermissionDenied
+		return super(self.__class__, self).get(self, request, *args, **kwargs)
 
 	def get_permissions(self):
 		permission_classes = [IsAuthenticated]
@@ -178,16 +194,21 @@ class BookingsViewSet(viewsets.ViewSet, mixins.ListModelMixin, viewsets.GenericV
 		# booked_property = Property.objects.get(id=pk)
 		if self.request.user.email == instance.client_email:
 			serializer_class = BookingUpdateClientSerializer
-		if self.request.user == instance.booked_by:
-			serializer_class = BookingUpdateAdminAndCreatorSerializer
-		else:
-			if instance.booked_property.owners.filter(user=self.request.user).exists():
+		owners = instance.booked_property.owners.all()
+		current_owner = None
+		for owner in owners:
+			if owner.user == self.request.user:
+				current_owner = owner
+
+		if current_owner:
+			if instance.booked_by == current_owner.user:
+				serializer_class = BookingUpdateAdminAndCreatorSerializer
+			if instance.booked_by != current_owner.user and current_owner.can_manage_bookings:
 				serializer_class = BookingUpdateAdminNotCreatorSerializer
 
 		serializer = serializer_class(
 			instance,
 			data=self.request.data,
-			partial=True,
 			context={'request': request, 'property_id': pk}
 		)
 		serializer.is_valid(raise_exception=True)
@@ -205,6 +226,22 @@ class BookingsViewSet(viewsets.ViewSet, mixins.ListModelMixin, viewsets.GenericV
 		instance.save()
 		return Response(status=status.HTTP_204_NO_CONTENT)
 
+	@action(detail=True, methods=['post'])
+	def cancel_booking(self, request, pk=None, booking_id=None):
+		# instead of deleting a row from db we set is_deleted = True
+		# after that a cron job with celery is set to remove the rows with is_deleted = True
+		# and transfer it into archive db
+
+		instance = self.get_object(booked_property=pk, booking_id=booking_id)
+		serializer = CancelBookingSerializer(
+			instance,
+			data=self.request.data,
+			context={'request': request, 'property_id': pk}
+		)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
 	def get_queryset(self):
 		return Booking.objects.all()
 
@@ -214,10 +251,9 @@ class BookingsViewSet(viewsets.ViewSet, mixins.ListModelMixin, viewsets.GenericV
 			permission_classes = [
 				CanRetrieve
 			]
-		if self.action == 'partial_update':
+		if self.action == 'update':
 			permission_classes = [
-				IsOwnerLevel100 | IsOwnerLevel200 | IsOwnerLevel300 |
-				IsOwnerLevel400 | IsClientOfBooking
+				CanUpdateBooking
 			]
 		if self.action == 'archive_booking':
 			permission_classes = [BookingIsAdminOfPropertyOrSuperuser]
@@ -226,7 +262,11 @@ class BookingsViewSet(viewsets.ViewSet, mixins.ListModelMixin, viewsets.GenericV
 	def get_object(self, booked_property=None, booking_id=None):
 		try:
 			obj = Booking.objects.prefetch_related(
-				'booked_property', 'booked_property__property_images', 'booked_property__property_address'
+				'booked_property',
+				'booked_property__property_images',
+				'booked_property__property_address',
+				'booked_property__owners__user',
+				'booked_property__owners'
 			).get(booked_property=booked_property, id=booking_id, is_deleted=False)
 			self.check_object_permissions(self.request, obj)
 			return obj
