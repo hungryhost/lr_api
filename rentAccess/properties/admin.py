ec04c1e53ed533175c1451ea8a7423fe85d7d688
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-03-24 19:41:17 +0300

    UPDATED: admin interface

--- rentAccess/properties/admin.py
+++ rentAccess/properties/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import (Property, PropertyLog, Ownership, PremisesImage, PremisesAddress, PropertyType,
-                     LockWithProperty, PermissionLevel, Availability)
+                     LockWithProperty, PermissionLevel, Availability, FavoriteProperty, AvailabilityException,
+Trip, TripMember, TripProperty
+
+                     )
 from bookings.models import Booking
 # Register your models here.
 app = 'properties'
@@ -11,17 +14,28 @@ class PropertiesMainAdmin(admin.ModelAdmin):
 
 
 class PropertiesAdmin(admin.ModelAdmin):
-	list_display = ('title', 'visibility', 'property_type')
+	list_display = ('id', 'title', 'visibility', 'property_type')
+
+
+class AvailabilityAdmin(admin.ModelAdmin):
+	list_display = ('id', 'premises')
 
 
 admin.site.register(Property, PropertiesAdmin)
-admin.site.register(Availability)
+admin.site.register(Availability, AvailabilityAdmin)
 admin.site.register(PremisesAddress)
 admin.site.register(PermissionLevel)
 admin.site.register(PropertyLog, PropertiesMainAdmin)
-admin.site.register(Ownership)
-admin.site.register(PremisesImage)
+admin.site.register(Ownership, AvailabilityAdmin)
+admin.site.register(PremisesImage, AvailabilityAdmin)
 admin.site.register(PropertyType)
 admin.site.register(Booking)
 admin.site.register(LockWithProperty)
+admin.site.register(FavoriteProperty)
+admin.site.register(AvailabilityException)
+admin.site.register(Trip)
+admin.site.register(TripMember)
+admin.site.register(TripProperty)
+
+
 
