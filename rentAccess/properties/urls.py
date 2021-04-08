f5e4ccc577cf60ee35944e46566a3444854ae212
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-04-09 00:45:25 +0300

    ADDED: new endpoints for availability exceptions

--- rentAccess/properties/urls.py
+++ rentAccess/properties/urls.py
@@ -4,6 +4,7 @@ from .views import PropertiesViewSet, PropertyListCreate, PropertyImagesViewSet,
 	LockList, OwnersListCreate, OwnershipViewSet, LocksViewSet
 from register.views import CardList, KeyList
 from bookings.views import BookingsListCreateView, BookingsViewSet, BookingsAllList
+from bookingCalendar.views import AvailabilityExceptionListCreateView
 
 # from .views import LockDetail
 app_name = 'properties'
@@ -11,35 +12,35 @@ app_name = 'properties'
 urlpatterns = [
 	path('', PropertyListCreate.as_view(), name='properties-list'),
 	path('<int:pk>/owners/<int:owner_id>/', OwnershipViewSet.as_view({
-		'get': 'retrieve',
-		'put': 'update',
+		'get'   : 'retrieve',
+		'put'   : 'update',
 		'delete': 'destroy'
 	}), name='properties-owners-details'),
 
 	path('<int:pk>/owners/', OwnersListCreate.as_view(), name='properties-owners-list'),
 	path('<int:pk>/', PropertiesViewSet.as_view({
-		'patch': 'partial_update',
-		'put': 'update',
-		'get': 'retrieve',
+		'patch' : 'partial_update',
+		'put'   : 'update',
+		'get'   : 'retrieve',
 		'delete': 'delete_property'
 	}), name='properties-details'),
 	path('<int:pk>/favorite/', PropertiesViewSet.as_view({
-		'put': 'add_to_favorite',
+		'put'   : 'add_to_favorite',
 		'delete': 'remove_from_favorite'
 	}), name='properties-favorite'),
 	path('<int:pk>/bookings/', BookingsListCreateView.as_view(), name='properties-bookings-list'),
 	path('<int:pk>/images/', PropertyImagesViewSet.as_view(
 		{
-			'get': 'list',
-			'put': 'update_property_pictures',
+			'get'   : 'list',
+			'put'   : 'update_property_pictures',
 			'delete': 'delete_images',
 		}
 	), name='properties-images-list'),
 
 	path('<int:pk>/bookings/<int:booking_id>/', BookingsViewSet.as_view(
 		{
-			'get': 'retrieve',
-			'put': 'update',
+			'get'   : 'retrieve',
+			'put'   : 'update',
 			'delete': 'archive_booking'
 		}
 	), name='properties-bookings-detail'),
@@ -50,10 +51,11 @@ urlpatterns = [
 	), name='properties-bookings-detail-cancel'),
 	path('<int:pk>/available/', PropertiesViewSet.as_view({
 		'post': 'get_availability',
-		'get': 'get_hourly_availability'
+		'get' : 'get_hourly_availability'
 	}), name='properties-availability-check'),
 	path('bookings/', BookingsAllList.as_view(), name='properties-bookings-list-all'),
-
+	path('<int:pk>/availability-exceptions/', AvailabilityExceptionListCreateView.as_view(),
+	     name='properties-availability-exceptions'),
 	path('<int:pk>/images/set_main_image/', PropertiesViewSet.as_view(
 		{
 			'put': 'change_main_image',
@@ -74,10 +76,10 @@ urlpatterns = [
 	)),
 	path('<int:pk>/locks/<int:lock_id>/cards/', CardList.as_view(), name='properties-cards-list'),
 	path('<int:pk>/locks/<int:lock_id>/keys/', KeyList.as_view(), name='properties-keys-list'),
-	#path('<int:pk>/images/<int:image_id>/', PropertyImagesViewSet.as_view(
+	# path('<int:pk>/images/<int:image_id>/', PropertyImagesViewSet.as_view(
 	#	{
 	#		'delete': 'delete_image'
 	#	}
-	#), name='properties-images-details'),
+	# ), name='properties-images-details'),
 
 ]
