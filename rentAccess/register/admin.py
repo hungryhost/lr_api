bdb763a8766ed231d9a96b73fad1278fef024fb6
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-04-20 02:13:24 +0300

    UPDATED: minor fixes

--- rentAccess/register/admin.py
+++ rentAccess/register/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from rest_framework_api_key.admin import APIKeyModelAdmin
 
-from .models import Key, Lock, Card, LockAPIKey
+from .models import Key, Lock, Card, LockAPIKey, LockIPAddress
 
 # Register your models here.
 
 admin.site.register(Key)
 admin.site.register(Lock)
 admin.site.register(Card)
+admin.site.register(LockIPAddress)
 
 @admin.register(LockAPIKey)
 class OrganisationAPIKeyModelAdmin(APIKeyModelAdmin):
